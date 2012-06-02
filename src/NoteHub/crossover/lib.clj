d4d2cc244b3b20413d3681f28f6f55d4f403e361
Christian Mueller <chmllr@me.com>
2012-06-02 13:08:55 +0200

    hash function seems to be stable now

--- src/NoteHub/crossover/lib.clj
+++ src/NoteHub/crossover/lib.clj
@@ -5,13 +5,12 @@
 ; (doesn't work for UTF-16!)
 (defn hash [f s]
   (let [short-mod #(mod % 32767)
-        ; we cannot use Math/pow because it's imprecise
-        ; and differs on JVM and JS
-        pow (fn [n e]
-              (reduce #(short-mod (* % %2)) 1
-                      (repeat e n)))
-        char-codes (map #(f (str %)) s)]
+        char-codes (map f
+                        (filter #(not (contains? #{"\n" "\r"} %)) (map str s)))]
     (reduce
       #(short-mod (+ % 
-                     (short-mod (* (first %2) (pow 31 (second %2))))))
+                     (short-mod (* (first %2) 
+                                   ((if (odd? %)
+                                      bit-xor
+                                      bit-and) 16381 (second %2))))))
       0 (map list char-codes (range)))))
