4ffaf3da40bb37563afbb6b36ade3f425cadd5bb
Christian Mueller <chmllr@me.com>
2012-06-10 22:03:49 +0200

    server is started in prod mode on heroku -> prev. rel. changes reverted

--- Makefile
+++ Makefile
@@ -1,9 +1,3 @@
-# This one is necessary to start the app in :dev mode, because
-# I changed the default mode to the production, because AFAIK
-# it's not possible to parameterize the app start on Heroku.
-run:
-	lein run dev
-
 server:
 	lein cljsbuild auto &
 	redis-server &
--- src/NoteHub/server.clj
+++ src/NoteHub/server.clj
@@ -4,7 +4,7 @@
 (server/load-views "src/NoteHub/views/")
 
 (defn -main [& m]
-  (let [mode (keyword (or (first m) :prod))
+  (let [mode (keyword (or (first m) :dev))
         port (Integer. (get (System/getenv) "PORT" "8080"))]
     (server/start port {:mode mode
                         :ns 'NoteHub})))
