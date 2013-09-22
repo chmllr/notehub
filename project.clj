3c082c7b6b060bddf131e141ccc069c9942305b2
Christian Mueller <chmllr@me.com>
2013-09-22 16:02:45 +0200

    clojure version downgraded

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject NoteHub "0.1.0-SNAPSHOT"
             :description "A free and anonymous hosting for markdown pages."
-            :dependencies [[org.clojure/clojure "1.4.0"]
+            :dependencies [[org.clojure/clojure "1.3.0"]
                            [org.clojure/clojure-contrib "1.2.0"]
                            [hiccup "1.0.0"]
                            [ring/ring-core "1.1.0"]
