cdbe779c5f74dcc3508b0a0a0ad6fcffc028a057
Christian Mueller <chmllr@me.com>
2014-01-29 20:37:15 +0100

    lein version added

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
                  [ring/ring-core "1.1.0"]
                  [com.taoensso/carmine "2.4.4"]
                  [compojure "1.1.6"]]
+  :min-lein-version "2.0.0"
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler notehub.handler/app}
   :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
