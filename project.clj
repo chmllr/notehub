e9e0b32366e6cf5225ad9d7067505171064947ed
Christian Müller <chmllr@me.com>
2014-10-05 13:05:36 +0200

    auto-refresh & auto-reload moved to dev profile only

--- project.clj
+++ project.clj
@@ -1,23 +1,23 @@
 (defproject NoteHub "2.0.0"
-  :description "A free and anonymous hosting for markdown pages."
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/core.cache "0.6.4"]
-                 [hiccup "1.0.5"]
-                 [zeus "0.1.0"]
-                 [org.pegdown/pegdown "1.4.2"]
-                 [iokv "0.1.1"]
-                 [cheshire "5.3.1"]
-                 [ring "1.3.1"]
-                 [com.taoensso/carmine "2.7.0" :exclusions  [org.clojure/clojure]]
-                 [compojure "1.2.0"]]
-  :main notehub.handler
-  :min-lein-version "2.0.0"
-  :plugins [[lein-ring "0.8.12"]]
-  :ring {:handler notehub.handler/app
-         :auto-reload? true
-         :auto-refresh? true}
-  :profiles {:uberjar {:aot :all}
-             :dev {:dependencies 
-                   [[javax.servlet/servlet-api "2.5"]
-                    [ring-mock "0.1.5"]]}}
-  :jvm-opts ["-Dfile.encoding=utf-8"])
+            :description "A free and anonymous hosting for markdown pages."
+            :dependencies [[org.clojure/clojure "1.6.0"]
+                           [org.clojure/core.cache "0.6.4"]
+                           [hiccup "1.0.5"]
+                           [zeus "0.1.0"]
+                           [org.pegdown/pegdown "1.4.2"]
+                           [iokv "0.1.1"]
+                           [cheshire "5.3.1"]
+                           [ring "1.3.1"]
+                           [com.taoensso/carmine "2.7.0" :exclusions  [org.clojure/clojure]]
+                           [compojure "1.2.0"]]
+            :main notehub.handler
+            :min-lein-version "2.0.0"
+            :plugins [[lein-ring "0.8.12"]]
+            :ring {:handler notehub.handler/app}
+            :profiles {:uberjar {:aot :all}
+                       :dev {:ring {:auto-reload? true
+                                    :auto-refresh? true}
+                             :dependencies
+                                   [[javax.servlet/servlet-api "2.5"]
+                                    [ring-mock "0.1.5"]]}}
+            :jvm-opts ["-Dfile.encoding=utf-8"])
