da3535e06ce698a58545c22dae82a551d14cd010
Christian Müller <no@way.click>
2015-09-30 20:21:03 +0200

    styling improved

--- resources/public/js/publishing.js
+++ resources/public/js/publishing.js
@@ -4,27 +4,7 @@ var timer = null;
 var timerDelay = iosDetected ? 800 : 400;
 var show = function(elem) { elem.style.display = "block" }
 var $note, $action, $preview, $plain_password, $input_elems, $dashed_line, $proposed_title, updatePreview;
-var firstLines_;
 var backendTimer;
-
-function updateProposedTitle() {
-    clearTimeout(backendTimer);
-    backendTimer = setTimeout(function () {
-        var http = new XMLHttpRequest();
-        var url = "/propose-title";
-        http.open("POST", url, true);
-        http.onreadystatechange = function() {
-            if(http.readyState == 4 && http.status == 200) {
-                var now = new Date();
-                $proposed_title.innerHTML = 
-        "Expected URL: https://www.notehub.org/" + 
-        now.getFullYear() + "/" + (now.getMonth()+1) + "/" + now.getDate() + "/" +
-        http.responseText;
-            }
-        }
-        http.send($note.value);
-    }, 500);
-}
 
 function md2html(input){
   return marked(input);
@@ -46,11 +26,6 @@ function onLoad () {
       show($dashed_line);
       show($input_elems);
       $preview.innerHTML = md2html(content);
-      var firstLines = content.split("\n", 2);
-      if(firstLines_ != firstLines) {
-          firstLines_ = firstLines;
-          updateProposedTitle();
-      }
     }, delay);
   };
   if($action){
