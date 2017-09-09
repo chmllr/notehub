8f12a1a6918558d082496489700de38f698731db
Christian Müller <no@way.click>
2017-09-09 20:49:45 +0200

    templating added

--- server.go
+++ server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"html/template"
+	"io"
 	"io/ioutil"
 	"net/http"
 
@@ -8,26 +10,31 @@ import (
 	"github.com/russross/blackfriday"
 )
 
+type Template struct{ templates *template.Template }
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
 func main() {
 	e := echo.New()
-
+	e.Renderer = &Template{templates: template.Must(template.ParseGlob("assets/templates/*.html"))}
 	e.Static("/", "assets/public")
-
-	e.GET("/Demo.md", func(c echo.Context) error { return c.String(http.StatusOK, mdPage(c, "Demo")) })
-	e.GET("/TOS.md", func(c echo.Context) error { return c.String(http.StatusOK, mdPage(c, "TOS")) })
-
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+	e.GET("/TOS.md", func(c echo.Context) error { return c.Render(http.StatusOK, "Page", md2html(c, "TOS")) })
 	e.Logger.Fatal(e.Start(":3000"))
 }
 
-func mdPage(c echo.Context, name string) string {
+type Note struct {
+	ID, Title string
+	Content   template.HTML
+}
+
+func md2html(c echo.Context, name string) *Note {
 	path := "assets/markdown/" + name + ".md"
 	mdContent, err := ioutil.ReadFile(path)
 	if err != nil {
 		c.Logger().Errorf("couldn't open markdown page %q: %v", path, err)
-		return ""
+		return nil
 	}
-	return string(blackfriday.Run(mdContent))
+	return &Note{Content: template.HTML(string(blackfriday.Run(mdContent)))}
 }
